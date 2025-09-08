a51480010e06616be5c06dad80162fb3c665f4a6
ajartijaouad <ajartijaouad14@gmail.com>
2025-09-08 13:15:20 +0200

    ajout du jeu de devinette : saisie lettre, affichage du mot caché et gestion des essais

--- main.c
+++ main.c
@@ -4,19 +4,45 @@ int main() {
     char lettre;
     char mot_fixe[6] = {'n', 'a', 'd', 'o', 'r', '\0'}; 
     char mot_Cache[6] = {'_', '_', '_', '_', '_', '\0'}; 
-    
-    printf("\t -- Jeu de Devinette de Caracteres --\n");
-
+    int trouve;
+    int essais_faux = 0; 
+    printf("\t -- Jeux de Devinette de Caracteres --\n");
     printf("\nBienvenue !\n");
-    printf("Le mot se compose de :\t");
-    
-    for ( int i = 0; i < 5; i++) {
+
+    do {
+         printf("Le mot se compose de :\t");
+        for (int i = 0; i < 5; i++) {
+            printf("%c ", mot_Cache[i]);
+        }
+        printf("\n");
+        printf("Entrez une lettre : ");
+        scanf(" %c", &lettre);
+        trouve = 0;
+        for (int i = 0; i < 5; i++) {
+            if (mot_fixe[i] == lettre) {
+                mot_Cache[i] = lettre;
+               
+            }
+        }
+
+        if (trouve) {
+            printf("Bravo ! Vous avez trouve une lettre : '%c'\n", lettre);
+        } else {
+                        essais_faux++;
+            printf("desole, la lettre '%c' n'est pas dans le mot.\n\n", lettre);
+        }
+         if (essais_faux == 5) {
+            printf(" Game Over .\n");
+            break;
+        }
+
+    } while (!trouve);
+
+    printf("\n Le Mot est aprer votre bon sasier : ");
+    for (int i = 0; i < 5; i++) {
         printf("%c ", mot_Cache[i]);
     }
     printf("\n");
-
-    printf("Entrez une lettre : ");
-    scanf(" %c", &lettre);
 
     return 0;
 }
